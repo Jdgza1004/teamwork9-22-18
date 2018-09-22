ba2d462510530a72bbe3e2cdce063ef28bb11608
Jennifer G <jennifer.garza2691@gmail.com>
2018-09-22 21:10:26 +0000

    with snipit

--- array.rb
+++ array.rb
@@ -11,3 +11,23 @@ break if value ==
 =end
 print "So I am not so sure what we really need to do I will try a arrray"
 print "I will write put numbers related to something random"
+
+
+def main
+  # we need an array to store our data
+  # how do we assign an array to a variable?
+  loop do
+    # continue to ask for a number
+    # challenge: how do I check that may input is an integer and not a string?
+    break if value == '' # this will end the loop if the user input is blank
+  end
+  # output data
+end
+
+main if __FILE__ == $PROGRAM_NAME
+
+
+
+Collapse 
+
+
