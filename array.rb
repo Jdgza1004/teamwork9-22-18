b74c2cdf7d544bd4187512aacf40609b0fee6da7
Jennifer G <jennifer.garza2691@gmail.com>
2018-09-22 20:10:47 +0000

    the powerpoint edition

---
+++ array.rb
@@ -0,0 +1,12 @@
+=begin
+def main
+-we need an array to store our data
+-how do we assign an array to a variable
+loop do
+-continue to ask for a number
+-challenge: how do I check that may input is an integer and not a string?
+break if value == 
+-this will end the loop if the user input is blank end
+-output data
+=end
+print "So I am not so sure what we really need to do I will try a arrray"
